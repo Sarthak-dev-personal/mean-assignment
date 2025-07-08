6696a12ed63b02ae5359c1efef1704e232ff3c13
sarthak-dev-personal <sarthak.kapoor4@gmail.com>
2025-07-08 18:47:12 +0530

    Downgraded Angular version and installed required dependencies

--- mean-ui/src/app/app.config.ts
+++ mean-ui/src/app/app.config.ts
@@ -1,16 +1,15 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
-} from '@angular/core';
-
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideStore(),
+    provideEffects(),
   ]
 };
